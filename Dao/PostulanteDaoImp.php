3648940f680256bf4e87a42c98fca3428fb207ce
CETECOM <CETECOM@LC021019>
2018-06-08 11:50:02 -0300

    asdsad

--- Dao/PostulanteDaoImp.php
+++ Dao/PostulanteDaoImp.php
@@ -1,6 +1,25 @@
 <?php
 
+include_once '../Sql/ClasePdo.php';
+include_once './BaseDao.php';
+include_once '../Dto/PostulanteDto.php';
 
-class PostulanteDaoImp {
-    
+class PostulanteDaoImp implements BaseDao {
+
+    public static function agregar($dto) {
+        
+    }
+
+    public static function eliminar($key) {
+        
+    }
+
+    public static function listarTodos() {
+        
+    }
+
+    public static function modificar($dto) {
+        
+    }
+
 }
--- Dto/UsuarioDto.php
+++ Dto/UsuarioDto.php
@@ -1,15 +1,21 @@
 <?php
 
-
 class UsuarioDto {
+
     private $rut;
     private $contrasena;
-    
+    private $nombre;
+    private $ap_materno;
+    private $ap_paterno;
+
     function __construct() {
         $this->rut = "";
         $this->contrasena = "";
+        $this->nombre = "";
+        $this->ap_materno = "";
+        $this->ap_paterno = "";
     }
-    
+
     function getRut() {
         return $this->rut;
     }
@@ -26,6 +32,28 @@ class UsuarioDto {
         $this->contrasena = $contrasena;
     }
 
+    function getNombre() {
+        return $this->nombre;
+    }
 
+    function getAp_materno() {
+        return $this->ap_materno;
+    }
+
+    function getAp_paterno() {
+        return $this->ap_paterno;
+    }
+
+    function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+
+    function setAp_materno($ap_materno) {
+        $this->ap_materno = $ap_materno;
+    }
+
+    function setAp_paterno($ap_paterno) {
+        $this->ap_paterno = $ap_paterno;
+    }
 
 }
