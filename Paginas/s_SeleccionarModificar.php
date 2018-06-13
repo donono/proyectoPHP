073143b21c2f5d5d21ca940145aec18b3ed0a906
mfrod <mfrod@DESKTOP-IVPHNN1>
2018-06-13 11:16:35 -0400

    modificar funcionando

---
+++ Paginas/s_SeleccionarModificar.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once '../Dao/PostulanteDaoImp.php';
+include_once '../Dao/SolicitudDaoImp.php';
+
+// Prueba de commit
+$rut = $_POST["rutModificar"];
+
+$salida = PostulanteDaoImp::BuscarPorRut($rut);
+$solicitud = SolicitudDaoImp::ListarPorRut($rut);
+session_start();
+$_SESSION["salida"] =  PostulanteDaoImp::BuscarPorRut($rut);
+$_SESSION["solicitud"] = SolicitudDaoImp::ListarPorRut($rut);
+
+include_once 'v_ModificarSolicitud.php';
+
